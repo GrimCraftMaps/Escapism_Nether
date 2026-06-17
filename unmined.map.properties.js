0c21caef2da3f47dfb4650507a9704ec567434da
grim <grim@localhost>
2026-06-17 01:46:19 -0400

    Nether render 2026-06-17

--- unmined.map.properties.js
+++ unmined.map.properties.js
@@ -4,7 +4,7 @@ var UnminedMapProperties = {
     maxZoom: 3,
     defaultZoom: 0,
     imageFormat: "jpeg",
-    minRegionX: -2,
+    minRegionX: -3,
     minRegionZ: -3,
     maxRegionX: 2,
     maxRegionZ: 1,
